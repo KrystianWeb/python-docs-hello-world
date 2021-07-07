8e7919e90372a3b7b74defe6e369e7d62947444e
Krystian.W <krystian.witoszkin@ebicom.pl>
2021-07-07 13:21:41 +0200

    method for squared number

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/squared", methods=['POST'])
+def squared_value():
+    if 'number' in request.args:
+        number = int(request.args['number'])
+    else:
+        return "Error: No number field provided. Please specify a number."
+    
+    return jsonify(number*number)
+
+app.run()
