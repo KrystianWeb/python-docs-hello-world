df3cf7943c3a36a7591473c273200bc394ee6dde
Krystian.W <krystian.witoszkin@ebicom.pl>
2021-07-08 12:16:17 +0200

    simple page

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+#---------dummy data----------
+list = [
+    {
+        'author': 'Author 1',
+        'title': 'Title 1',
+        'content': 'Bla bla bla',
+        'date': 'Date 1'
+    },
+    {
+        'author': 'Author 2',
+        'title': 'Title 2',
+        'content': 'Bla bla bla 2',
+        'date': 'Date 2'
+    },
+    {
+        'author': 'Author 3',
+        'title': 'Title 3',
+        'content': 'Bla bla bla 3',
+        'date': 'Date 3'
+    }
 
-@app.route("/squared", methods=['POST'])
+]
+#-----------------------------
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', list=list, title='TEST')
+
+@app.route("/squared", methods=['POST','GET'])
 def squared_value():
     if 'number' in request.args:
         number = int(request.args['number'])
@@ -14,4 +39,5 @@ def squared_value():
     
     return jsonify(number*number)
 
-#app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
